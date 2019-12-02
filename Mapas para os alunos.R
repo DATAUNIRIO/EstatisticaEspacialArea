b11598d0e80b85847e10cec7365ae10dc68471bb
Steven <steven.ross@uniriotec.br>
2019-12-02 05:46:14 -0300

    shapefile

--- Mapas para os alunos.R
+++ Mapas para os alunos.R
@@ -40,3 +40,28 @@ tm_shape(base) +
   tm_facets(sync = TRUE, ncol = 2)
 
 
+#------------------------------------------------------------------------------
+
+
+library(rgdal)
+shape <-readOGR("C:/Users/Steven/Downloads/RJ-MUN/33MUE250GC_SIR.shp")
+plot(shape) # plota o mapa do Brasil
+View(shape)
+
+library(readxl)
+Mun_RJ <- read_excel("~/GitHub/Base_de_dados/BasesMunicipios.xlsx")
+View(Mun_RJ)
+
+## Relaciona os estados do "banco" com as informações dos "dados"
+base_para_mapa <- merge(shape,Mun_RJ,by.x="CD_GEOCODM",by.y="COD IBGE2")
+
+dim(base_para_mapa)
+
+library(tmap)
+
+tmap_mode("view")
+
+tm_shape(base_para_mapa) + tm_fill("ISS", style="jenks",palette="YlOrRd") +
+  tm_borders() + tm_layout(frame=F)
+
+
---
+++ tidygeocoder.R
@@ -0,0 +1,32 @@
+library(dplyr)
+library(tidygeocoder)
+
+dc_addresses <- tribble( ~name,~addr,
+                         "White House", "1600 Pennsylvania Ave Washington, DC",
+                         "National Academy of Sciences", "2101 Constitution Ave NW, Washington, DC 20418",
+                         "Department of Justice", "950 Pennsylvania Ave NW, Washington, DC 20530",
+                         "Supreme Court", "1 1st St NE, Washington, DC 20543",
+                         "Washington Monument", "2 15th St NW, Washington, DC 20024")
+
+coordinates <- dc_addresses %>%
+  geocode(addr)
+
+coordinates
+
+library(OpenStreetMap)
+dc_map <- openmap( c(38.905,-77.05),c(38.885,-77.00))
+dc_map.latlng <- openproj(dc_map)
+
+library(ggplot2)
+library(ggrepel)
+autoplot(dc_map.latlng) +
+  theme_minimal() +
+  theme(      axis.text.y=element_blank(),
+              axis.title=element_blank(),
+              axis.text.x=element_blank(),
+              plot.margin = unit(c(0, 0, 0, 0), "cm")
+  ) +
+  geom_point(data=coordinates, aes(x=long, y=lat), color="navy", size=4, alpha=1) +
+  geom_label_repel(data=coordinates,
+                   aes(label=name,x=long, y=lat),show.legend=F,box.padding=.5,size = 5)
+
